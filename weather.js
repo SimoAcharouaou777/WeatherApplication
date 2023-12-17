74212b0d87d72d8eeadc6459a3cb1934669347b9
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-12-17 15:12:03 +0100

    its working

--- weather.js
+++ weather.js
@@ -1,14 +1,40 @@
 const apiKey = "5848cef567f8ca5af68bf4abd2d461dd";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=morocco";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city) {
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+     if(response.status == 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display ="none";
+     }
+     else{
+        document.querySelector(".weather").style.display ="block";
+     }
     var data = await response.json();
-    console.log(data);
-    document.querySelector(".city").innerHTML= data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    // console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    }
+
 }
-checkWeather();
+        searchbtn.addEventListener("click", () => {
+            checkWeather(searchbox.value);
+        })
