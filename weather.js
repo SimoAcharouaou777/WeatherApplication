f6a8b288d6f51d12132725d06af85cca3f50f963
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-12-17 13:49:43 +0100

    start displaying the weather info

--- weather.js
+++ weather.js
@@ -6,6 +6,9 @@ async function checkWeather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
-
+    document.querySelector(".city").innerHTML= data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
 }
 checkWeather();
